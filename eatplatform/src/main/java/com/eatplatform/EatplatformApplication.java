793518bfb2bd7d1deba87dfbd581f55462e993fd
DevHsu <dlsrks7676@gmail.com>
2025-04-14 14:49:13 +0900

    Spring Boot Open JDK 17.0.2 Project DI(Spring Boot DevTools, Spring Web, Loombok, Spring Security, MyBatis, MySQL Driver, JavaMailSender)

---
+++ eatplatform/src/main/java/com/eatplatform/EatplatformApplication.java
@@ -0,0 +1,13 @@
+package com.eatplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatplatformApplication.class, args);
+	}
+
+}
